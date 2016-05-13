af589f93f3fd96b4f1639d037282d5c926baecca
Daniel Gollings <Daniel.Gollings@ing.nl>
2016-05-13 12:24:34 +0200

    more robust way to show ip

--- scripts/bootmaster.sh
+++ scripts/bootmaster.sh
@@ -287,5 +287,4 @@ echo "*             SHOW IP                          *"
 echo "*                                              *"  
 echo "************************************************"  
 echo
-
-ip addr | grep 192
+ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}'
--- scripts/bootnode.sh
+++ scripts/bootnode.sh
@@ -76,4 +76,4 @@ echo "*                                              *"
 echo "************************************************"  
 echo
 
-ip addr | grep 192
+ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}'
